c5de7fa7da45b0d0898836e39fe2046875d61d8c
MrOtherGuy <jastekken@outlook.com>
2020-08-05 13:20:29 +0300

    Set the prefix based on the type of the page loaded in active tab

--- src/background-script.js
+++ src/background-script.js
@@ -3,11 +3,58 @@
   
   // Store home and newtab urls
   // This won't be updated during runtime so if you change those you'll need to restart Firefox
-  const BROWSER_URLS = new Array(4);
-  browser.browserSettings.newTabPageOverride.get({}).then(result=>(BROWSER_URLS[0] = result.value));
-  browser.browserSettings.homepageOverride.get({}).then(result=>(BROWSER_URLS[1] = result.value));
-  BROWSER_URLS[2] = "about:privatebrowsing";
-  BROWSER_URLS[3] = "about:blank";
+  const BROWSER_URLS = new(function(){
+    
+    const urls = new Array(4);
+    urls[2] = "about:privatebrowsing";
+    urls[3] = "about:blank";
+
+    this.match = function(tab){
+      let idx = urls.indexOf(tab.url);
+      if(idx >= 0){
+        switch (idx){
+          case 0:
+            return "NewTab"
+          case 1:
+            return "Home"
+          case 2:
+            return "PrivateBrowsing"
+          case 3:
+            return "Blank"
+          default:
+            return ""
+        }
+      }
+      if(tab.url.startsWith("about:") || tab.url.startsWith("chrome://")){
+        return "chromeUI"
+      }
+      if(tab.url.startsWith("https:")){ return "" }
+      
+      return (tab.url.slice(0,tab.url.indexOf(":")))
+      
+    }      
+    this.setInitials = (ntp,home) => { urls[0] = ntp.value, urls[1] = home.value };
+    
+    return this
+  })()
+  
+  const activeNewtabs = new Map();
+  
+  Promise.all([
+    browser.browserSettings.newTabPageOverride.get({}),
+    browser.browserSettings.homepageOverride.get({})
+  ])
+  .then(results=>{
+    BROWSER_URLS.setInitials(results[0],results[1])
+  })
+  .then(()=>(new Promise(r=>{setTimeout(r,500)})))
+  .then(()=>browser.tabs.query({active:true}))
+  .then(tabs=>{
+    if(!tabs){ return }
+    for(let tab of tabs){
+      onTabUpdated(tab.id,{},tab);
+    }
+  });
   
   function NewTab(tab){
     this.id = tab.id;
@@ -15,7 +62,6 @@
     return this
   }
   
-  let activeNewtabs = new Map();
   
   browser.tabs.onCreated.addListener(a=>{
     activeNewtabs.set(a.id,new NewTab(a))
@@ -25,14 +71,21 @@
     setTimeout(()=>(activeNewtabs.delete(a)),500)
   });
   
-  browser.tabs.onUpdated.addListener((tabId,info,tab)=>{
-    if(activeNewtabs.has(tabId)){
-      if(!BROWSER_URLS.includes(tab.url)){
-        activeNewtabs.delete(tabId);
-        browser.windows.update(tab.windowId,{titlePreface:""});
-      }
+  function onTabUpdated(tabId,info,tab){
+    //const isNewtab = BROWSER_URLS.includes(tab.url);
+    const tabType = BROWSER_URLS.match(tab);
+    const isKnown = activeNewtabs.has(tabId);
+    if(/*isNewtab*/tabType && !isKnown){
+      // This happens when navigation occurs to ntp tab without creating a tab
+      activeNewtabs.set(tabId,new NewTab(tab));
+    }else if(isKnown && tabType != "Blank"){
+      activeNewtabs.delete(tabId);
     }
-  },{properties:["title"]});
+    //browser.windows.update(tab.windowId,{titlePreface:isNewtab?"NewTab":""});
+    browser.windows.update(tab.windowId,{titlePreface:tabType.length>3?`${tabType} - `:""})
+  }
+  
+  browser.tabs.onUpdated.addListener(onTabUpdated,{properties:["title"]});
   
   function setProperties(tab,tabId){
     let match = tab === null ? tabId : tab.id;
@@ -41,10 +94,14 @@
     if(win === undefined || win === null){
       return
     }
+    
+    let prefix = !tab?"NewTab":`${BROWSER_URLS.match(tab)} - `;
 
-    const str = (!tab || BROWSER_URLS.includes(tab.url)) ? "NewTab" : "";
 
-    browser.windows.update(win,{titlePreface:str});
+    //const str = (!tab || BROWSER_URLS.includes(tab.url)) ? "NewTab" : "";
+    //browser.windows.update(win,{titlePreface:str});
+    //const str = `${!tab?"NewTab":BROWSER_URLS.match(tab)} - `;
+    browser.windows.update(win,{titlePreface:prefix.length > 3?prefix:""});
   }
   
   function onActivated(tabInfo){
@@ -52,7 +109,9 @@
     getting.then(setProperties,(err)=>(setProperties(null,tabInfo.tabId)));
   }
   // Timeout to make sure this happens after onCreated
-  browser.tabs.onActivated.addListener( (a)=>setTimeout(onActivated(a),100));
+  // and also to make sure this runs after a tab is loaded in new window since it will initially likely load about:blank and we can't reliably detect when about:blank will be replaced with the eventual content
+  browser.tabs.onActivated.addListener((t)=>setTimeout(()=>onActivated(t),100));
+ 
   
 })()
 
